555f06b2484f30f7093dd1ba703c1f6f975ca8a1
Your Name <you@example.com>
2025-10-30 15:48:39 -0400

    ...

--- scripts/nodes/interactive.gd
+++ scripts/nodes/interactive.gd
@@ -3,30 +3,68 @@
 signal toggled()
 signal toggled_on()
 signal toggled_off()
-signal hold_started()
-signal hold_percent(amnt: float)
-signal hold_ended()
+signal charge_started()
+signal charge_percent(amnt: float)
+signal charge_ended()
 signal interacted(controllable: Controllable, form: Form)
 signal mounted(controller: Controllable)
 signal unmounted(controller: Controllable)
 
 enum Form { INTERACT, ENTERED, EXITED }
+enum ToggleIterationMode { FORWARD, BACKWARD, RANDOM }
+
 @export var label: String = "Interact"
 var can_interact := func(_controllable: Controllable): return true
 
 @export_group("Toggleable")
+@export var toggleable := false:
+	get: return toggle_states != 0
+	set(t):
+		if t:
+			if toggle_states == 0:
+				toggle_states = 1
+		else:
+			toggle_states = 0
+@export var on := false:
+	get: return toggle_state != 0
+	set(o):
+		if o:
+			if toggle_state == 0:
+				toggle_state = 1
+		else:
+			toggle_state = 0
 @export var toggle_states := 0 ## 0 == no toggle. 1 = on/off.
-@export var toggle_state := 0
+@export var toggle_state := 0:
+	set(t):
+		toggle_state = t
+		if toggle_state == 0:
+			toggled_off.emit()
+		else:
+			toggled_on.emit()
+		toggled.emit()
+@export var toggle_labels: PackedStringArray = ["Interact [Turn Off]", "Interact [Turn On]"]
+@export var toggle_iteration_mode := ToggleIterationMode.FORWARD ## Only when toggle_states > 0.
+@export var toggle_iteration_loop := true ## Loop or clamp.
+var toggle_count := 0 ## How many times interactive was toggled.
+var _toggle_next := 0 ## Next state we will enter. Needed for setting accurate labels.
 
-@export_group("Holdable")
-@export var hold_time := 0.0 ## How many seconds to hold interact. Useful for turnwheels.
-@export var hold_instant_reset := true
-@export var hold_increase_scale := 1.0
-@export var hold_decrease_scale := 1.0
-var _held := false
-var _held_time := 0.0
-var _held_controllable: Controllable
-var _held_form: Form
+@export_group("Chargeable")
+@export var chargeable := false: ## Based on charge_time state. 0.0 == false.
+	get: return charge_time != 0.0
+	set(c):
+		if c:
+			if charge_time == 0.0:
+				charge_time = 1.0
+		else:
+			charge_time = 0.0
+@export var charge_time := 0.0 ## Seconds to hold interact down.
+@export var charge_instant_reset := true
+@export var charge_increase_scale := 1.0
+@export var charge_decrease_scale := 1.0
+var _charging := false
+var _charge_time := 0.0
+var _charge_controllable: Controllable
+var _charge_form: Form
 
 @export_group("Enterable & Exitable")
 @export var interact_on_enter := false ## Interaction occurs when object enters.
@@ -52,9 +90,6 @@ func _ready() -> void:
 	body_entered.connect(_entered)
 	body_exited.connect(_exited)
 	set_process(false)
-
-func is_toggled() -> bool: return toggle_state >= 1
-func is_toggleable() -> bool: return toggle_states >= 1
 
 func is_mounted() -> bool:
 	return mounted_controllable != null
@@ -113,57 +148,63 @@ func _exited(body: Node3D):
 
 func interact(controllable: Controllable, form := Form.INTERACT):
 	if not can_interact.call(controllable): return
-	if hold_time == 0:
+	if charge_time == 0:
 		_interacted(controllable, form)
 	else:
-		_held = true
-		_held_controllable = controllable
-		_held_form = form
-		hold_started.emit()
+		_charging = true
+		_charge_controllable = controllable
+		_charge_form = form
+		charge_started.emit()
 		set_process(true)
 
 ## Cancel the interaction.
 ## Only used when hold_time != 0.0
 func cancel(controllable: Controllable):
-	if _held and controllable == _held_controllable:
-		_held = false
-		_held_controllable = null
-		hold_ended.emit()
+	if _charging and controllable == _charge_controllable:
+		_charging = false
+		_charge_controllable = null
+		charge_ended.emit()
 
 func _process(delta: float) -> void:
-	if _held:
-		_held_time += delta * hold_increase_scale
-		if _held_time >= hold_time:
+	if _charging:
+		_charge_time += delta * charge_increase_scale
+		if _charge_time >= charge_time:
 			set_process(false)
-			_held_time = hold_time
-			hold_percent.emit(1.0)
-			_interacted(_held_controllable, _held_form)
-			_held_controllable = null
-		else:
-			hold_percent.emit(remap(_held_time, 0.0, hold_time, 0.0, 1.0))
-	
+			_charge_time = charge_time
+			_interacted(_charge_controllable, _charge_form)
+			_charge_controllable = null
 	else:
-		if hold_instant_reset:
-			_held_time = 0.0
+		if charge_instant_reset:
+			_charge_time = 0.0
 		else:
-			_held_time -= delta * hold_decrease_scale
-		if _held_time <= 0.0:
+			_charge_time -= delta * charge_decrease_scale
+		
+		if _charge_time <= 0.0:
 			set_process(false)
-			_held_time = 0.0
-			hold_percent.emit(0.0)
-		else:
-			hold_percent.emit(remap(_held_time, 0.0, hold_time, 0.0, 1.0))
+			_charge_time = 0.0
+	
+	var percent := remap(_charge_time, 0.0, charge_time, 0.0, 1.0)
+	charge_percent.emit(percent)
+	print(percent)
 
 func _interacted(controllable: Controllable, form: Form) -> void:
 	interacted.emit(controllable, form)
 	
-	if is_toggleable():
-		toggle_state = wrapi(toggle_state + 1, 0, toggle_states)
-		toggled.emit()
-		if is_toggled():
-			toggled_on.emit()
+	if toggleable:
+		toggle_count += 1
+		toggle_state = _toggle_next
+		match toggle_iteration_mode:
+			ToggleIterationMode.FORWARD: _toggle_next += 1
+			ToggleIterationMode.BACKWARD: _toggle_next -= 1
+			ToggleIterationMode.RANDOM: _toggle_next = randi() % toggle_states
+			
+		if toggle_iteration_loop:
+			_toggle_next = wrapi(_toggle_next, 0, toggle_state)
 		else:
-			toggled_off.emit()
+			_toggle_next = clampi(_toggle_next, 0, toggle_states)
+		
+		# Label is what happens *next*
+		label = toggle_labels[_toggle_next]
 	
 	if mountable:
 		mount(controllable)
--- scripts/nodes/toggle_node.gd
+++ scripts/nodes/toggle_node.gd
@@ -1,20 +1,45 @@
-@tool
-extends Node
+@tool extends Node
 
 @export var interactive: Interactive: set=set_interactive
-@export var on := false
+@export var on := true:
+	get: return interactive.toggle_state == on_state if interactive else false
+	set(o):
+		if on_state == 0:
+			on_state = 1
+		else:
+			on_state = 0
+@export var on_state := 0:
+	set(s):
+		on_state = s
+		_toggled()
 @export_custom(PROPERTY_HINT_EXPRESSION, "") var expr_on := ""
 @export_custom(PROPERTY_HINT_EXPRESSION, "") var expr_off := ""
 
-func set_interactive(inter: InteractiveToggle):
-	if interactive: interactive.interacted.disconnect(_toggled)
+func set_interactive(inter: Interactive):
+	if interactive:
+		interactive.toggled.disconnect(_toggled)
+	if inter == null:
+		var parent := get_parent()
+		if parent is Interactive:
+			inter = parent
 	interactive = inter
-	if interactive: interactive.interacted.connect(_toggled)
+	if interactive:
+		interactive.toggled.connect(_toggled)
+	print("connected")
 
-func _toggled(_cont: Controllable, _form: Interactive.Form):
-	var exx := expr_on if on else expr_off
+func _toggled():
+	if on:
+		_execute_script(expr_on)
+	else:
+		_execute_script(expr_off)
+
+func _execute_script(expr: String):
+	if not is_inside_tree(): return
 	var gd := GDScript.new()
-	gd.source_code = "static var this\nstatic func _run(): %s" % [exx.replace("$", "this.")]
-	gd.reload()
+	gd.source_code = "@tool\nstatic var this\nstatic func _run(): %s" % [expr.replace("$", "this.")]
+	var err := gd.reload()
+	if err != OK:
+		push_error(self, error_string(err))
+		return
 	gd.this = self
 	gd._run()
